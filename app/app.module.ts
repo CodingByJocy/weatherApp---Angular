07e4533e48abd979a7ef27d01646c38061f76439
CodingByJocy <117545031+CodingByJocy@users.noreply.github.com>
2023-06-16 21:40:00 +0200

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { WeatherService } from './weather/weather.service';
+import {HttpClientModule} from '@angular/common/http'
+import { ICurrentWeather } from './interfaces';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WeatherComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [WeatherService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/environment/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    baseUrl: 'https://api.openweathermap.org/data/2.5/weather?q={Belgrade}&appid={c5558a75eeb65bed547ad40103355770}',
+    appId: 'c5558a75eeb65bed547ad40103355770'
+
+}
