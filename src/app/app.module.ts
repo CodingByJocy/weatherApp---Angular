851f5509d2f41bc9e1c1f0260a91d3075cacbf70
CodingByJocy <117545031+CodingByJocy@users.noreply.github.com>
2023-06-17 19:15:21 +0200

    Add files via upload
    issues fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { WeatherService } from './weather/weather.service';
+import {HttpClientModule} from '@angular/common/http'
+import { ICurrentWeather } from './interfaces';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ICurrentWeather } from '../interfaces';
 import { WeatherService } from './weather.service';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -10,15 +11,29 @@ import { WeatherService } from './weather.service';
 })
 export class WeatherComponent implements OnInit {
   current: ICurrentWeather | any;
-  weatherData: ICurrentWeather | any; 
+  weatherData: ICurrentWeather | any;
+  cityName: string = '';
 
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit() {
     this.weatherService.getCurrentWeather('Belgrade')
       .subscribe((data) => {
-        this.current = data;
-        this.weatherData = data; // Assign the received data to the weatherData property
+        this.current = data.name;
+        this.weatherData = data;
       });
   }
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      const city = this.cityName;
+      this.weatherService.getCurrentWeather(city)
+        .subscribe((data) => {
+          this.current = data.name;
+          this.weatherData = data;
+        });
+      form.reset();
+    }
+  }
 }
+
